216618c8a348a469ff52c8e2893f010fad759a63
SH248212 <SH248212@falmouth.ac.uk>
2021-12-06 13:42:03 +0000

    Work on collisions and pawns

---
+++ UnrealWork/Intermediate/Build/Win64/UE4Editor/Inc/UnrealWork/CollidingPawn.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UNREALWORK_CollidingPawn_generated_h
+#error "CollidingPawn.generated.h already included, missing '#pragma once' in CollidingPawn.h"
+#endif
+#define UNREALWORK_CollidingPawn_generated_h
+
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_SPARSE_DATA
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_RPC_WRAPPERS
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACollidingPawn(); \
+	friend struct Z_Construct_UClass_ACollidingPawn_Statics; \
+public: \
+	DECLARE_CLASS(ACollidingPawn, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/UnrealWork"), NO_API) \
+	DECLARE_SERIALIZER(ACollidingPawn)
+
+
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesACollidingPawn(); \
+	friend struct Z_Construct_UClass_ACollidingPawn_Statics; \
+public: \
+	DECLARE_CLASS(ACollidingPawn, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/UnrealWork"), NO_API) \
+	DECLARE_SERIALIZER(ACollidingPawn)
+
+
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACollidingPawn(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACollidingPawn) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACollidingPawn); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACollidingPawn); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACollidingPawn(ACollidingPawn&&); \
+	NO_API ACollidingPawn(const ACollidingPawn&); \
+public:
+
+
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACollidingPawn(ACollidingPawn&&); \
+	NO_API ACollidingPawn(const ACollidingPawn&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACollidingPawn); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACollidingPawn); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACollidingPawn)
+
+
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_PRIVATE_PROPERTY_OFFSET
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_9_PROLOG
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_PRIVATE_PROPERTY_OFFSET \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_SPARSE_DATA \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_RPC_WRAPPERS \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_INCLASS \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define UnrealWork_Source_UnrealWork_CollidingPawn_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_PRIVATE_PROPERTY_OFFSET \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_SPARSE_DATA \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_INCLASS_NO_PURE_DECLS \
+	UnrealWork_Source_UnrealWork_CollidingPawn_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UNREALWORK_API UClass* StaticClass<class ACollidingPawn>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID UnrealWork_Source_UnrealWork_CollidingPawn_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- UnrealWork/Intermediate/Build/Win64/UE4Editor/Inc/UnrealWork/UnrealWork.init.gen.cpp
+++ UnrealWork/Intermediate/Build/Win64/UE4Editor/Inc/UnrealWork/UnrealWork.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeUnrealWork_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x8A82CF7A,
-				0x39EC8B87,
+				0x021CE67D,
+				0x4C548D14,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
